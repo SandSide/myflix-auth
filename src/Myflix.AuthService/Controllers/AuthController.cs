7bec6059d87c3a0d6a36235ef1560eea7e3b2b3d
SandSide <37347875+SandSide@users.noreply.github.com>
2024-01-02 22:56:54 +0000

    feat: add Login action method to  AuthController

---
+++ src/Myflix.AuthService/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Myflix.AuthService.Models;
+
+namespace Myflix.AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return Ok("Signed In");
+            }
+
+            return Unauthorized();
+        }
+    }
+}
