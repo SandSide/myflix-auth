f63acc4ee889d8c7bdbc2b9416ce07c465af76c9
SandSide <37347875+SandSide@users.noreply.github.com>
2024-01-02 22:54:07 +0000

    feat: set up local db

--- src/Myflix.AuthService/Data/ApplicationDbContext.cs
+++ src/Myflix.AuthService/Data/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Myflix.AuthService.Models;
 
 namespace Myflix.AuthService.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {     
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
     }
